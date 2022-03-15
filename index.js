9af60bd3740ba04e64f52537a336bed2877e6ea6
Jordan <jmwilliams94@icloud.com>
2022-03-15 18:49:43 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(name, message) {
+
+    let cards = [];
+    for (let i = 0; i < name.length; i++) {
+        cards.push(`Thank you, ${name[i]}, for the wonderful ${message} gift!`);
+        
+      }
+    return cards
+}
+
+function countDown(n) {
+    for (let countDown = n; countDown >= 0; countDown--) {
+        console.log(countDown);
+      } 
+}
+countDown(10);
